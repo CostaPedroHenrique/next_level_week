77cd63ca1b5530b5697be0456ea735de30605bf2
CostaPedroHenrique <devpedrohenrique@gmail.com>
2020-06-05 12:16:47 -0300

    Fronted iniciado

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import Routes from './routes';
 function App() {
   return (
-    <h1>Ola, mundo!</h1>
+    <Routes/>
+    
   );
 }
+
 
 export default App;
